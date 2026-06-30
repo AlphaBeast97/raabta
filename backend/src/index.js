8adccde9ecbe672880a21fa60e20b225da9279f9
AlphaBeast97 <ksaad5272@gmail.com>
2026-07-01 00:23:21 +0500

    feat: implement MongoDB connection and add message and user models

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { clerkMiddleware } from "@clerk/express";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 dotenv.config();
@@ -8,6 +9,11 @@ app.use(clerkMiddleware());
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is running on port ${PORT}`);
 });
